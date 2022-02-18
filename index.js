9b23c417adc57941324d92878482101c0de4e2b1
Marko Vukovic <marko@marko.vukovic>
2022-02-18 13:40:20 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const getRepos = async ({
+  username = 'markovuk992',
+  page = 1,
+  per_page = 30
+} = {}) => {
+  try {
+    const repos = await axios.get(
+      `https://api.github.com/users/${username}/repos?page=${page}&per_page=${per_page}&sort=updated`
+    );
+    return repos.data
+      .map((repo) => {
+        return {
+          name: repo.name,
+          url: repo.html_url,
+          description: repo.description,
+          stars: repo.stargazers_count
+        };
+      })
+      .sort((first, second) => second.stars - first.stars);
+  } catch (error) {
+    return [];
+  }
+};
+
+//getRepos().then((repositories) => console.log(repositories));
+/*getRepos({
+	 username: 'markovuk992',
+	 page: 1, 
+	 per_page: 10
+}).then((repositories) => console.log(repositories));
+*/
+
+module.exports = { getRepos };
